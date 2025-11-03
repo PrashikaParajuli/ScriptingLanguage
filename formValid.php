b1696446d22080cf833222bcb137cfc82a15eb68
PrashikaParajuli <prashikaparajuli69@gmail.com>
2025-11-03 09:37:23 +0545

    form validation

---
+++ formValid.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        username: <input type="text" class="text"><br>
+        Password: <input type="text" class="password"><br>
+        Confirm password: <input type="text" class="conf-password">
+    </form>
+</body>
+</html>
